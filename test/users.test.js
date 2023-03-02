8a117edd65bef299677b43f3f1f49b0d96039316
Andrea Ángel García <andrea.angel@adaits.es>
2023-03-02 18:01:42 +0100

    Actualización antes de mejoras en clase

--- test/users.test.js
+++ test/users.test.js
@@ -4,10 +4,11 @@ const jest = require('jest');
 const userService = require('../src/services/mongodb-service/user');
 const User = require('../src/models/user');
 
+// TEST GETALLUSERS
 describe('UserService', () => {
   describe('getAllUsers', () => {
     it('debería devolver todos los usuarios', async () => {
-      const mockUsers = [{ name: 'Juan' }, { name: 'Pedro' }];
+      const mockUsers = [{ name: 'Andrea' }, { name: 'Manolillo' }];
       User.find = jest.fn().mockResolvedValue(mockUsers);
 
       const result = await userService.getAllUsers();
@@ -18,8 +19,8 @@ describe('UserService', () => {
     });
 
     it('debería devolver los usuarios filtrados', async () => {
-      const mockUsers = [{ name: 'Juan' }];
-      const mockFilters = { name: 'Juan' };
+      const mockUsers = [{ name: 'Andrea' }];
+      const mockFilters = { name: 'Gabri' };
       User.find = jest.fn().mockResolvedValue(mockUsers);
 
       const result = await userService.getAllUsers(mockFilters);
@@ -28,5 +29,86 @@ describe('UserService', () => {
       expect(User.find).toHaveBeenCalledWith(mockFilters);
       expect(result).toEqual(mockUsers);
     });
+  });
+});
+
+// TEST CREATEUSER
+describe('createUser', () => {
+  it('debería crear un usuario correctamente', async () => {
+    const body = {
+      name: 'Juan',
+      email: 'juan@example.com',
+      password: 'secretpassword',
+    };
+
+    const createdUser = await createUser(body);
+
+    expect(createdUser).toBeDefined();
+    expect(createdUser.name).toEqual('Juan');
+    expect(createdUser.email).toEqual('juan@example.com');
+    expect(createdUser.password).not.toEqual('secretpassword');
+  });
+
+  it('debería lanzar un error si se proporciona una entrada inválida', async () => {
+    const body = {
+      email: 'juan@example.com',
+      password: 'secretpassword',
+    };
+    await expect(createUser(body)).rejects.toThrow(); // se espera que se lance una excepción
+  });
+});
+
+// TEST UPDATEUSER
+describe('updateUser', () => {
+  it('debería actualizar un usuario correctamente', async () => {
+    const existingUser = new User({
+      name: 'Juan',
+      email: 'juan@example.com',
+      password: 'secretpassword',
+    });
+    await existingUser.save();
+
+    const updateData = {
+      name: 'Pedro',
+      password: 'newpassword',
+    };
+
+    const updatedUser = await updateUser({ email: existingUser.email, ...updateData });
+
+    expect(updatedUser).toBeDefined();
+    expect(updatedUser.name).toEqual('Pedro');
+    expect(updatedUser.email).toEqual('juan@example.com');
+  });
+
+  it('debería lanzar un error si se proporciona un correo electrónico no existente', async () => {
+    const updateData = {
+      name: 'Pedro',
+      password: 'newpassword',
+    };
+
+    await expect(updateUser({ email: 'noexiste@example.com', ...updateData })).rejects.toThrow();
+  });
+});
+
+// TEST BORRAR USUARIO
+describe('deleteUser', () => {
+  it('debería eliminar un usuario existente correctamente', async () => {
+    const existingUser = new User({
+      name: 'Juan',
+      email: 'juan@example.com',
+      password: 'secretpassword',
+    });
+    await existingUser.save();
+
+    const deletedUser = await deleteUser(existingUser.email);
+
+    expect(deletedUser).toBeDefined();
+    expect(deletedUser.deletedCount).toEqual(1);
+  });
+
+  it('debería lanzar un error si se proporciona un correo electrónico no existente', async () => {
+    const nonExistingEmail = 'noexiste@example.com';
+
+    await expect(deleteUser(nonExistingEmail)).rejects.toThrow();
   });
 });
