f407e15294839ee1b6d642e3b2504c72f40ee45c
ManuelGuerraRuiz <andrea.angel@adaits.es>
2023-03-04 17:50:47 +0100

    Actualización rutas y swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 
 const { rootController } = require('../controllers');
 
@@ -10,15 +11,14 @@ const smtpController = require('../controllers/smtp');
 // const { jwtAuth } = require('../middleware/auth-middleware');
 const LoginController = require('../controllers/loginController');
 const adminAuth = require('../middleware/admin-middleware');
-
-const router = express.Router();
+const { createUser } = require('../controllers/user');
 
 router.post('/', rootController);
-router.post('/logIn', LoginController.logIn);
+router.post('/login', LoginController.logIn);
+router.post('/signin', createUser);
 // router.post('/login', authController.logIn);
 // router.post('/signin', createUser);
 // router.use(jwtAuth);
-
 router.use('/admin', adminAuth, AdminRouter);
 router.use('/users', UserRouter);
 router.use('/booking', BookingRouter);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {createUser} = require('../controllers/user');
-
 const { authMiddleware } = require('../middleware/auth-middleware');
 const { itsMe } = require('../middleware/auth-middleware');
 const UserController = require('../controllers/user');
-
-router.post('', createUser);
 
 router.get('', authMiddleware, itsMe, UserController.getUsers);
 router.put('/:email', authMiddleware, itsMe, UserController.updateUser);
