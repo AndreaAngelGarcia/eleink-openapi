32ccfb37e24f69f51a198d9565a63ea1720f5d4e
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-06 12:57:33 +0200

    Actualización cors

--- PROYECTO/src/index.js
+++ PROYECTO/src/index.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const app = require('./app');
 const config = require('./config');
 const { logger } = require('./utils');
 
 const { port } = config;
 
+app.use(cors());
 app.listen(port, err => {
   if (err) {
     logger.error(err);
