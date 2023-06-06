508463d0bbb6d51b45c0a5040a026dee0fa3fcbb
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-06 13:03:59 +0200

    Update index.js

--- PROYECTO/src/index.js
+++ PROYECTO/src/index.js
@@ -3,9 +3,9 @@ const app = require('./app');
 const config = require('./config');
 const { logger } = require('./utils');
 
+app.use(cors());
 const { port } = config;
 
-app.use(cors());
 app.listen(port, err => {
   if (err) {
     logger.error(err);
